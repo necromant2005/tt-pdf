900b8c5d9e488246e38f505788e89f99528cc0dd
Rostislav Mykhajliw <necromant2005@gmail.com>
2014-04-29 12:55:17 +0300

    report fix

--- src/TweePdf/Service/Report.php
+++ src/TweePdf/Service/Report.php
@@ -6,9 +6,7 @@ class Report
 {
     public function toPdf(array $configuration)
     {
-        $tmp = '/tmp/x.pdf';
         $pdf = new ZendPdf\PdfDocument();
-        $pdf->save($tmp);
-        return file_get_contents($tmp);
+        return $pdf->render();
     }
 }
