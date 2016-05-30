2ac55def9104d112ae0a436e7952341313139cee
Rostislav Mykhajliw <necromant2005@gmail.com>
2016-05-30 07:04:03 +0300

    fix pdf genration

--- src/TweePdf/Service/casperjs/html2pdf.js
+++ src/TweePdf/Service/casperjs/html2pdf.js
@@ -1,12 +1,10 @@
-var casper = require("casper").create({
-  viewportSize: {width: 768, height: 1024}
-});
+var casper = require("casper").create();
 
 input = casper.cli.get('input');
 output = casper.cli.get('output');
 
 casper.start();
-casper.viewport(768, 1024);
+casper.viewport(1024, 768);
 casper.page.paperSize = {
     width: '8.5in',
     height: '11in',
@@ -15,7 +13,7 @@ casper.page.paperSize = {
     border: '0.4in',
     margin: '0.4in'
 };
-casper.page.viewportSize = {width: 768, height: 1024};
+//casper.page.viewportSize = {width: 768, height: 1024};
 
 casper.on('remote.message', function(msg) {
     this.echo('remote message caught: ' + msg);
