bc3d45df6ce0fee7314ffdca7e898b2b3ca1f9d6
Rostislav Mykhajliw <necromant2005@gmail.com>
2017-04-18 22:16:38 +0300

    docker

--- src/TweePdf/Service/Html2Pdf.php
+++ src/TweePdf/Service/Html2Pdf.php
@@ -5,27 +5,33 @@ class Html2Pdf
 {
     public function convert(string $html) : string
     {
-        $tempnam = tempnam(sys_get_temp_dir(), 'converting-html-to-pdf-');
-        file_put_contents($tempnam, 'abc');
-        $htmlFile  = $tempnam . '.html';
-        $pdfFile   = $tempnam . '.pdf';
+        $tmpname = tempnam(sys_get_temp_dir(), 'converting-html-to-pdf-');
+        $dirname = $tmpname . '-d';
+        mkdir($dirname);
 
-        file_put_contents($htmlFile, $html);
+        // $dirname = __DIR__ .'/../../../tmp';
+        // @mkdir($dirname);
 
-        $cmd = '/usr/local/bin/casperjs '
-            . escapeshellarg('--ssl-protocol=TLSv1') . ' '
-            . escapeshellarg('--ignore-ssl-errors=yes') . ' '
-            . escapeshellarg(__DIR__ . '/casperjs/html2pdf.js') . ' '
-            . '--input=' . escapeshellarg($htmlFile) . ' '
-            . '--output=' . escapeshellarg($pdfFile);
+        copy(__DIR__ . '/casperjs/html2pdf.js', $dirname . '/html2pdf.js');
+        file_put_contents($dirname . '/input.html', $html);
+
+        $cmd = 'docker run --rm -v ' . $dirname . ':/mnt necromant2005/tt-pdf '
+            . '/usr/local/bin/casperjs '
+            . '--ssl-protocol=TLSv1 '
+            . '--ignore-ssl-errors=yes '
+            . '/mnt/html2pdf.js '
+            . '--input=/mnt/input.html '
+            . '--output=/mnt/output.pdf ';
 
         // echo $cmd . PHP_EOL;
-        @system($cmd);
-        $content = file_get_contents($pdfFile);
+        system($cmd);
+        $content = file_get_contents($dirname . '/output.pdf');
 
         @unlink($tempnam);
-        @unlink($htmlFile);
-        @unlink($pdfFile);
+        @unlink($dirname . '/html2pdf.js');
+        @unlink($dirname . '/input.html');
+        @unlink($dirname . '/output.pdf');
+        rmdir($dirname);
 
         return $content;
     }
--- tests/TweePdf/Service/Html2PdfTest.php
+++ tests/TweePdf/Service/Html2PdfTest.php
@@ -9,6 +9,6 @@ class Html2PdfTest extends PHPUnit_Framework_TestCase
         $service = new Html2Pdf();
         $content = $service->convert(file_get_contents(__DIR__ . '/_files/report.html'));
         $this->assertEquals('PDF', substr($content, 1, 3));
-        $this->assertGreaterThan(100 * 1000, strlen($content));
+        $this->assertGreaterThan(10 * 1000, strlen($content));
     }
 }
