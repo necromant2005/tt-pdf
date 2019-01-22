ca7e01655b1a57278af9aac1110188f476949778
Rostislav Mykhajliw <necromant2005@gmail.com>
2019-01-22 09:24:52 +0200

    fancy pdf renderiing

--- tests/TweePdf/Service/Html2PdfTest.php
+++ tests/TweePdf/Service/Html2PdfTest.php
@@ -9,6 +9,6 @@ class Html2PdfTest extends TestCase
         $service = new Html2Pdf();
         $content = $service->convert(file_get_contents(__DIR__ . '/_files/report.html'));
         $this->assertEquals('PDF', substr($content, 1, 3));
-        $this->assertGreaterThan(10 * 1000, strlen($content));
+        $this->assertGreaterThan(50 * 1000, strlen($content));
     }
 }
