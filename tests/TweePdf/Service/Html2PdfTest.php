efe13cb4837d42386bb99528ed8f14a9367f9284
Rostislav Mykhajliw <necromant2005@gmail.com>
2019-10-31 07:31:15 +0300

    add test for large file

--- tests/TweePdf/Service/Html2PdfTest.php
+++ tests/TweePdf/Service/Html2PdfTest.php
@@ -8,7 +8,15 @@ class Html2PdfTest extends TestCase
     {
         $service = new Html2Pdf();
         $content = $service->convert(file_get_contents(__DIR__ . '/_files/report.html'));
-        $this->assertEquals('PDF', substr($content, 1, 3));
         $this->assertGreaterThan(50 * 1000, strlen($content));
+        $this->assertEquals('PDF', substr($content, 1, 3));
+    }
+
+    public function testPdfLarge()
+    {
+        $service = new Html2Pdf();
+        $content = $service->convert(str_repeat(file_get_contents(__DIR__ . '/_files/report.html'), 10));
+        $this->assertGreaterThan(50 * 1000, strlen($content));
+        $this->assertEquals('PDF', substr($content, 1, 3));
     }
 }
