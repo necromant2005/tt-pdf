6fdf7dbac372fabf43d71f3a152247aaea8b0d29
Rostislav Mykhajliw <necromant2005@gmail.com>
2019-10-31 07:24:16 +0300

    Revert "move pdf rendering without directory mounting"
    This reverts commit 8aeace45cb82d41a103b86362edde2076965347d.

--- src/TweePdf/Service/Html2Pdf.php
+++ src/TweePdf/Service/Html2Pdf.php
@@ -3,19 +3,34 @@ namespace TweePdf\Service;
 
 class Html2Pdf
 {
-    private const SEPARATOR = 'GNsWoiq7mSOXX5KI2fKXIOy8PlMokuxQ7PAkVRh6xTTkOtREwp';
-
     public function convert(string $html) : string
     {
-        $cmd = 'docker run --rm pdf-docker /mnt/run.sh '
-            . base64_encode(file_get_contents(__DIR__ . '/casperjs/html2pdf.js')) . ' '
-            . base64_encode($html);
+        $dirname = '/tmp/' . uniqid() . uniqid() . uniqid() . uniqid();
+        mkdir($dirname);
+
+        // $dirname = __DIR__ .'/../../../tmp';
+        // @mkdir($dirname);
+
+        copy(__DIR__ . '/casperjs/html2pdf.js', $dirname . '/html2pdf.js');
+        file_put_contents($dirname . '/input.html', $html);
+
+        $cmd = 'docker run --rm -v ' . $dirname . ':/mnt truesocialmetrics/pdf-rendering '
+            . '/usr/local/bin/casperjs '
+            . '--ssl-protocol=TLSv1 '
+            . '--ignore-ssl-errors=yes '
+            . '/mnt/html2pdf.js '
+            . '--input=/mnt/input.html '
+            . '--output=/mnt/output.pdf ';
 
         // echo $cmd . PHP_EOL;
+        system($cmd);
+        $content = file_get_contents($dirname . '/output.pdf');
 
-        $content = (string) shell_exec($cmd);
-        $position = strpos($content, self::SEPARATOR);
+        @unlink($dirname . '/html2pdf.js');
+        @unlink($dirname . '/input.html');
+        @unlink($dirname . '/output.pdf');
+        rmdir($dirname);
 
-        return substr($content, $position + strlen(self::SEPARATOR) + 1);
+        return $content;
     }
 }
